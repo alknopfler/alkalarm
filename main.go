b819d1a507581e1856a3505d934768e0ad82715a
alberto.morgante <alberto.morgante@bbva.com>
2018-02-09 18:31:43 +0100

    fixing

--- main.go
+++ main.go
@@ -3,26 +3,22 @@ package main
 import (
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/alknopfler/alkalarm/database"
-	"github.com/alknopfler/alkalarm/sensors"
-	"os"
 )
 
-
-
 func init(){
+
 	fmt.Println("Validating the database, and other params...Could take some minutes...")
 	//First Time to execute needs create database and scheme
-	db,err := database.InitDB()
+	/*db,err := database.InitDB()
 	if err != nil {
 		fmt.Println("Error initiating DB")
 		os.Exit(2)
 	}
 	defer db.Close()
 	database.CreateSchemas(db)
+	*/
 	fmt.Println("Success...Starting the program")
 }
 
 func main() {
-	sensors.RegisterSensor()
 }
