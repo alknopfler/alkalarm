e98ec118443aa0277b6dfb4947255d111ea34f5c
alberto.morgante <alberto.morgante@bbva.com>
2018-02-10 23:26:16 +0100

    add discoverCode feature with menu interactive

--- utils/discoverCodes.go
+++ utils/discoverCodes.go
@@ -8,6 +8,7 @@ import (
 	"errors"
 	cfg "github.com/alknopfler/alkalarm/config"
 
+	"encoding/json"
 )
 
 func discoverCodeSensor() (string, error) {
@@ -37,14 +38,42 @@ func discoverCodeSensor() (string, error) {
 	return "",errors.New("Sensor Not Found after 10 second looking for it...")
 }
 
-func main(){
+func menu(){
+	fmt.Println("***********************************")
 	fmt.Println("Utility to discover sensor codes...")
 	fmt.Println("The program will stop automatically when it discover the sensor.")
-	fmt.Println("You could stop using ctrl+c")
-	code,err:=discoverCodeSensor()
-	if err!=nil{
-		fmt.Println("Fail: ",err)
-	}else{
-		fmt.Println("Code:",code)
+	fmt.Println("Anyway, you could stop using ctrl+c")
+	fmt.Println("***********************************")
+	var listSensor []cfg.Sensor
+	for true{
+		fmt.Println("Looking for new sensor...Try to activate manually to detect it...")
+		code,err:=discoverCodeSensor()
+
+		if err!=nil{
+			fmt.Println("Fail: ",err)
+		}else{
+			fmt.Println("Code Detected:",code)
+			fmt.Print("Enter type of Sensor [presence|aperture|other]: ")
+			var typeof string
+			fmt.Scanln(&typeof)
+			fmt.Print("Enter the zone of your house: ")
+			var zone string
+			fmt.Scanln(&zone)
+			listSensor = append(listSensor,cfg.Sensor{code,typeof,zone})
+		}
+		fmt.Print("Do you want to continuos with another sensor?:[Y|n] ")
+		var answer string
+		fmt.Scanln(&answer)
+		if answer == "n" || answer == "N"{
+			break
+		}
 	}
+	response, _ := json.Marshal(listSensor)
+	fmt.Println("The json with the sensors detected is: ")
+	fmt.Println(response)
+	fmt.Println("Try to register in AlkAlarm using the API setup/sensor and load the body payload with this json.")
+}
+
+func main(){
+	menu()
 }
