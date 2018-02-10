4a2739a013a1a64cdf2780640a18a7abcc36e1bb
alberto.morgante <alberto.morgante@bbva.com>
2018-02-10 20:51:02 +0100

    finished the sensor api features

--- api/sensor.go
+++ api/sensor.go
@@ -1,18 +1,53 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
+	"github.com/alknopfler/alkalarm/sensors"
+	"github.com/gorilla/mux"
+	cfg "github.com/alknopfler/alkalarm/config"
+)
 
 
 //HandlerCreateSensor function
 func HandlerCreateSensor(w http.ResponseWriter, r *http.Request) {
 	//TODO la alarma global debe estar desactivada para operar
+	input, err := readMailBodyJson(r)
+	if err != nil {
+		responseWithError(w, http.StatusBadRequest,err.Error())
+	}
+	for i:=range input{
+		err=sensors.RegisterSensor(input[i])
+		if err!= nil {
+			responseWithError(w,http.StatusInternalServerError,err.Error())
+			return
+		}
+	}
+	responseWithJSON(w,http.StatusCreated,"Sensor Registered successfully")
+
 }
 
 //HandlerDeleteSensor function
 func HandlerDeleteSensor(w http.ResponseWriter, r *http.Request) {
 	//TODO la alarma global debe estar desactivada para operar
+	codeInput, _ := mux.Vars(r)["code"]
+	if ! sensors.SensorExists(codeInput){
+		responseWithError(w, http.StatusBadGateway, "Sensor Not Found")
+		return
+	}
+	data:= cfg.Sensor{Code:codeInput}
+	err:=sensors.UnregisterSensor(data)
+	if err!=nil{
+		responseWithError(w,http.StatusInternalServerError,err.Error())
+		return
+	}
+	responseWithJSON(w,http.StatusOK,"Sensor Deleted successfully")
 }
 
 func HandlerGetSensors(w http.ResponseWriter, r *http.Request){
-
+	value,err:=sensors.QuerySensorsAll()
+	if err!=nil{
+		responseWithError(w,http.StatusInternalServerError,err.Error())
+		return
+	}
+	responseWithJSON(w,http.StatusOK,value)
 }
