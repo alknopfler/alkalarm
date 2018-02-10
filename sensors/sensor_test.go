e910523124c12313adf4f7f9dd1d0c4df89bab2a
alberto.morgante <alberto.morgante@bbva.com>
2018-02-10 11:01:46 +0100

    starting testing for sensor

--- sensors/sensor_test.go
+++ sensors/sensor_test.go
@@ -1,1 +1,15 @@
 package sensors
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"github.com/alknopfler/alkalarm/database"
+)
+
+func TestRegisterSensor(t *testing.T) {
+	db,err := database.InitDB("./test.db")
+	defer db.Close()
+	assert.NoError(t,err)
+
+	//RegisterSensor()
+}
