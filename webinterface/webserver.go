0978b55bc1637054025a293c99caf45246c316f7
alberto.morgante <alberto.morgante@bbva.com>
2018-02-13 22:59:10 +0100

    add webserver to develop the front

---
+++ webinterface/webserver.go
@@ -0,0 +1,16 @@
+
+
+package main
+
+import (
+"log"
+"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("webinterface"))
+	http.Handle("/", fs)
+
+	log.Println("Listening...")
+	http.ListenAndServe(":3000", nil)
+}
