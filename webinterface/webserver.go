87007454fec7db6cd1f423c85096476d68dbc6de
alberto.morgante <alberto.morgante@bbva.com>
2018-02-15 19:12:34 +0100

    add path to original func

--- webinterface/webserver.go
+++ webinterface/webserver.go
@@ -5,11 +5,10 @@ package main
 import (
 "log"
 "net/http"
-	"strings"
 )
 
-/*func main() {
-	fs := http.FileServer(http.Dir("./"))
+func main() {
+	fs := http.FileServer(http.Dir("./static/"))
 	http.Handle("/", fs)
 
 	log.Println("Listening...")
@@ -17,18 +16,4 @@ import (
 
 
 }
-*/
-func noDirListing(h http.Handler) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if strings.HasSuffix(r.URL.Path, "/") {
-			http.NotFound(w, r)
-			return
-		}
-		h.ServeHTTP(w, r)
-	})
-}
 
-func main() {
-	http.Handle("/", noDirListing(http.FileServer(http.Dir("./static/"))))
-	log.Println(http.ListenAndServe(":80", nil))
-}
