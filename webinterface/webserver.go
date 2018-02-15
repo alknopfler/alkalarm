dfbcd855c3d3092adb66cae745e454d9804a0aa7
alberto.morgante <alberto.morgante@bbva.com>
2018-02-15 19:07:10 +0100

    update webserver to remove dir listing

--- webinterface/webserver.go
+++ webinterface/webserver.go
@@ -5,12 +5,30 @@ package main
 import (
 "log"
 "net/http"
+	"strings"
 )
 
-func main() {
+/*func main() {
 	fs := http.FileServer(http.Dir("./"))
 	http.Handle("/", fs)
 
 	log.Println("Listening...")
 	http.ListenAndServe(":80", nil)
+
+
+}
+*/
+func noDirListing(h http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasSuffix(r.URL.Path, "/") {
+			http.NotFound(w, r)
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	http.Handle("/", noDirListing(http.FileServer(http.Dir("./"))))
+	log.Println(http.ListenAndServe(":80", nil))
 }
