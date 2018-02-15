114da0b5c66f6d558e6cadf36f96e79fc770f537
alberto.morgante <alberto.morgante@bbva.com>
2018-02-15 23:35:59 +0100

    reload document

--- webinterface/scripts/clear.js
+++ webinterface/scripts/clear.js
@@ -4,7 +4,7 @@
      url: 'http://alknopfler.ddns.net:8080/alarm',
      type: 'DELETE',
      success: function (data, textStatus, xhr) {
-         $('#table').reload()
+         $(document).reload()
      },
      error: function (xhr, textStatus, errorThrown) {
          console.log('Error in Operation');
