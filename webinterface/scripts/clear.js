d06efbf8e0fc166eea5b27503b0b084856ce3d13
alberto.morgante <alberto.morgante@bbva.com>
2018-02-16 09:54:52 +0100

    codding

---
+++ webinterface/scripts/clear.js
@@ -0,0 +1,13 @@
+$("#clear").click(function () {
+                     $.ajax({
+                         async: true,
+                         crossDomain: true,
+                         url: 'http://alknopfler.ddns.net:8080/alarm',
+                         type: 'DELETE',
+                         success: function () {
+                            //if (data.success){
+                                   $("#table").load();
+                                   console.log("Entra success")
+                         }
+                     });
+                 });
