e20e7f8d9161ee9fa83ae9ac20972c27d0d9dc15
alberto.morgante <alberto.morgante@bbva.com>
2018-02-16 09:10:52 +0100

    creating 2 colors for text in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "   _               _
+tput setaf 1;echo "   _               _
    / \  | | | __   / \  | | __ _ _ __ _ __ ___
   / _ \ | | |/ /  / _ \ | |/ _' | '__| '_ ' _ \
  / ___ \| |   <  / ___ \| | (_| | |  | | | | | |
@@ -41,47 +41,65 @@ function create_folder(){
 ##### main #####
 
 tput setaf 2;echo "Starting the installation...Please press a key:"
-read
+tput sgr0;read
 
-tput setaf 2;echo "### Installing packages ... ###"
+tput setaf 2;echo
+echo "### Installing packages ... ###"
+tput sgr0;echo
 apt-get -y update
 apt-get -y install golang python-pip python-dev pigpio python-pigpio python3-pigpio
 
-tput setaf 2;echo "### Creating Folders ... ###"
+tput setaf 2;echo
+echo "### Creating Folders ... ###"
+tput sgr0;echo
 create_folder $PROJECT_PATH
 create_folder $TMP_PATH
 create_folder $LOG_PATH
 
-tput setaf 2;echo "### Copy the python script and binary to the project directory ... ### "
+tput setaf 2;echo
+echo "### Copy the python script and binary to the project directory ... ### "
+tput sgr0;echo
 go install alkalarm.go
 cp _433.py /root/bin/alkalarm $PROJECT_PATH/
 
-tput setaf 2;echo "### Installing web server and copy the binary to the project directory ... ###"
+tput setaf 2;echo
+echo "### Installing web server and copy the binary to the project directory ... ###"
+tput sgr0;echo
 go install webinterface/webserver.go
 cp /root/bin/webserver $PROJECT_PATH/
 
-
-tput setaf 2;echo "### Creating the password file with email smtp server password ... ### "
-read -p "Introduce la password de la cuenta smtp: " password
+tput setaf 2;echo
+echo "### Creating the password file with email smtp server password ... ### "
+tput sgr0;read -p "Introduce la password de la cuenta smtp: " password
 echo $password>$PROJECT_PATH/.passSMTP
 
-tput setaf 2;echo "### Enabling the PiGPIO daemon ###"
+tput setaf 2;echo
+echo "### Enabling the PiGPIO daemon ###"
+tput sgr0;echo
 systemctl enable pigpiod
 
-tput setaf 2;echo "### Creating the file in rsyslog ###"
+tput setaf 2;echo
+echo "### Creating the file in rsyslog ###"
+tput sgr0;echo
 cp 30-alkalarm.conf /etc/rsyslog.d/
 systemctl restart rsyslog
 
-tput setaf 2;echo "### Creating AlkAlarm systemd service ###"
+tput setaf 2;echo
+echo "### Creating AlkAlarm systemd service ###"
+tput sgr0;echo
 cp alkalarm.service /lib/systemd/system/.
 chmod 755 /lib/systemd/system/alkalarm.service
 systemctl enable alkalarm
 systemctl start alkalarm
 
-tput setaf 2;echo "### Creating webserver systemd service ###"
-cp alkalarm.service /lib/systemd/system/.
-chmod 755 /lib/systemd/system/alkalarm.service
-systemctl enable alkalarm
-systemctl start alkalarm
+tput setaf 2;echo
+echo "### Creating webserver systemd service ###"
+tput sgr0;echo
+cp alkalarm-webserver.service /lib/systemd/system/.
+chmod 755 /lib/systemd/system/alkalarm-webserver.service
+systemctl enable alkalarm-webserver
+systemctl start alkalarm-webserver
 
-tput setaf 2;echo "### End Of Installation...SUCCESS ###"
+tput setaf 2;echo
+echo "### End Of Installation...SUCCESS ###"
+tput sgr0;echo
