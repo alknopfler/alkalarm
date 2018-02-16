1eb7278710f4ffbacc7c85962b8e46a41f0ca5df
alberto.morgante <alberto.morgante@bbva.com>
2018-02-16 15:26:17 +0100

    change setup

--- webinterface/scripts/setup.js
+++ webinterface/scripts/setup.js
@@ -9,38 +9,38 @@ $(document).ready(function () {
                  $.ajax({
                      async: true,
                      crossDomain: true,
-                     url: 'http://192.168.10.70:8080/setup/sensor',
+                     url: 'http://alknopfler.ddns.net:8080/setup/sensor',
                      type: 'POST',
                      dataType: 'json',
-                     data: sensor.serialize(),
+                     data: sensor,
                      success: function (data, textStatus, xhr) {
                          console.log(data);
                      },
                  });
              });
              $("#saveControl").click(function () {
-                  var sensor = $('#control').val();
+                  var control = $('#control').val();
                   $.ajax({
                       async: true,
                       crossDomain: true,
-                      url: 'http://192.168.10.70:8080/setup/control',
+                      url: 'http://alknopfler.ddns.net:8080/setup/control',
                       type: 'POST',
                       dataType: 'json',
-                      data: sensor,
+                      data: control,
                       success: function (data, textStatus, xhr) {
                           console.log(data);
                       },
                   });
              });
              $("#saveMailer").click(function () {
-                   var sensor = $('#mailer').val();
+                   var mailer = $('#mailer').val();
                    $.ajax({
                        async: true,
                        crossDomain: true,
-                       url: 'http://192.168.10.70:8080/setup/mail',
+                       url: 'http://alknopfler.ddns.net:8080/setup/mail',
                        type: 'POST',
                        dataType: 'json',
-                       data: sensor,
+                       data: mailer,
                        success: function (data, textStatus, xhr) {
                            console.log(data);
                        },
