e5027a675c606ecaf0bf90e6471027cd48fd5c01
alberto.morgante <alberto.morgante@bbva.com>
2018-02-16 10:18:43 +0100

    join into a file js

--- webinterface/scripts/tables.js
+++ webinterface/scripts/tables.js
@@ -21,3 +21,18 @@ function drawRow(rowData) {
     row.append($("<td>" + rowData.Date + "</td>"));
     row.append($("<td>" + rowData.Sensor + "</td>"));
 }
+
+$("#clearAlarm").click(function () {
+                    console.log("entra por aqui")
+                     $.ajax({
+                         async: true,
+                         crossDomain: true,
+                         url: 'http://alknopfler.ddns.net:8080/alarm',
+                         type: 'DELETE',
+                         success: function () {
+                            //if (data.success){
+                                   $("#table").load();
+                                   console.log("Entra success")
+                         }
+                     });
+                 });
