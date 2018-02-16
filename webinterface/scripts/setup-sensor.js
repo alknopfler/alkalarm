ffbbb2c69791631b496c9adba9345abc6e8497e0
alberto.morgante <alberto.morgante@bbva.com>
2018-02-16 16:41:54 +0100

    create subsensor page

---
+++ webinterface/scripts/setup-sensor.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+ var code = prompt("Please enter your code:", "");
+
+ jQuery.get('../../.htaccess', function(data) {
+ if (code ==data) {
+
+             $("#saveSensor").click(function () {
+                 var sensor = $('#inputjson').val();
+                 $.ajax({
+                     async: true,
+                     crossDomain: true,
+                     url: 'http://alknopfler.ddns.net:8080/setup/sensor',
+                     type: 'POST',
+                     dataType: 'json',
+                     data: sensor,
+                     success: function (data, textStatus, xhr) {
+                         window.alert("Sensor loaded successfully");
+                     },
+                     error: function (data){
+                         window.alert("Remember to stop the alarm before...");
+                     }
+                 });
+             });
+             $("#clearAlarm").click(function () {
+                  var code = $('#code').val();
+                  $.ajax({
+                      async: true,
+                      url: 'http://alknopfler.ddns.net:8080/setup/sensor/'+code,
+                      type: 'delete',
+                      success: function () {
+                         window.alert("Sensor loaded successfully");
+                         $("#sensortable").load(" #sensortable")
+                      },
+                      error: function (data){
+                           window.alert("Remember to stop the alarm before...");
+                      }
+                  });
+             });
+             $("#sensortable").show(function (){
+                     $.ajax({
+                         url: 'http://alknopfler.ddns.net:8080/setup/sensor',
+                         type: "get",
+                         dataType: "json",
+                         data: '',
+                         success: function(data, textStatus, jqXHR) {
+                             // since we are using jQuery, you don't need to parse response
+                             for (var i = 0; i < data.length; i++) {
+                                 var row = $("<tr />")
+                                 $("#sensortable").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
+                                 row.append($("<td>" + data[i].Code + "</td>"));
+                                 row.append($("<td>" + data[i].TypeOf + "</td>"));
+                                 row.append($("<td>" + data[i].Zone + "</td>"));
+
+                             }
+                         }
+                     });
+                 });
+ }
+ });
+ });
