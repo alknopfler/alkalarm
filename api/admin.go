868fe1949e9085cab306763e39848fc3e52eed64
Alberto Morgante <alberto.morgante@orangex.es>
2018-11-17 19:00:08 +0100

    not sense the admin check

--- api/admin.go
+++ api/admin.go
@@ -2,18 +2,16 @@ package api
 
 import (
 	"net/http"
-	"github.com/gorilla/mux"
-	cfg "github.com/alknopfler/alkalarm/config"
 )
 
 
 //HandlerDeleteMail function
 func HandlerVerifyPass(w http.ResponseWriter, r *http.Request) {
-	receptorInput, _ := mux.Vars(r)["pass"]
-	if receptorInput != cfg.WEBACCESS_PASS{
-		responseWithError(w, http.StatusBadGateway, "Bad Password")
-		return
-	}
+	//receptorInput, _ := mux.Vars(r)["pass"]
+	//if receptorInput != cfg.WEBACCESS_PASS{
+	//	responseWithError(w, http.StatusBadGateway, "Bad Password")
+	//	return
+	//}
 	responseWithJSON(w,http.StatusOK,"Good Password")
 
 }
